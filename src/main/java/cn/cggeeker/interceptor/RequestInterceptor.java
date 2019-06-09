400f123401a1849e1cca04cf42bb895a53da29a3
CGgeeker <2574691263@qq.com>
2019-06-09 17:50:15 +0800

    push

---
+++ src/main/java/cn/cggeeker/interceptor/RequestInterceptor.java
@@ -0,0 +1,33 @@
+package cn.cggeeker.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Auther:CG
+ * @Date:2019/6/6/006
+ * @Description:com.itlike.interceptor
+ * @version:1.0
+ */
+@Slf4j
+public class RequestInterceptor implements HandlerInterceptor {
+    /*private static final Logger log = LoggerFactory.getLogger(MyInterceptor.class); */ //加入Lombok的@Slf4j注解省略此句，但是我的失效了
+
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.debug("RequestInterceptor----------preHandle拦 - 截 - 器 - 处 - 理 - 中........");
+
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+    }
+
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+    }
+}
---
+++ src/main/java/cn/cggeeker/service/impl/HeroServiceImpl.java
@@ -0,0 +1,28 @@
+package cn.cggeeker.service.impl;
+
+
+import cn.cggeeker.mapper.HeroMapper;
+import cn.cggeeker.pojo.Hero;
+import cn.cggeeker.service.HeroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @Auther:CG
+ * @Date:2019/6/9/009
+ * @Description:cn.cggeeker
+ * @version:1.0
+ */
+@Service
+public class HeroServiceImpl implements HeroService {
+    @Autowired
+    private HeroMapper heroMapper;
+
+    @Override
+    public List<Hero> getAllHero() {
+        System.out.println(":::service:::"+heroMapper.selectAll());
+        return heroMapper.getAllHero();
+    }
+}
