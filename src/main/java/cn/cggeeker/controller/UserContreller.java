ff2324dd6e43c69248740823d3cb5011b6c64357
CGgeeker <2574691263@qq.com>
2019-06-10 23:45:28 +0800

    update  --- something

--- src/main/java/cn/cggeeker/controller/UserContreller.java
+++ src/main/java/cn/cggeeker/controller/UserContreller.java
@@ -1,8 +1,11 @@
 package cn.cggeeker.controller;
 
+import cn.cggeeker.pojo.User;
 import cn.cggeeker.service.UserService;
+import cn.cggeeker.util.ResultJson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,11 +16,25 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @Slf4j
+@RequestMapping("/user")
 public class UserContreller {
     @Autowired
     private UserService userService;
 
-
+    @RequestMapping("/userLoginValidate")
+    public ResultJson userLoginValidate(String userName,String passWord){
+        ResultJson resultJson = new ResultJson();
+        User user = userService.userLoginValidate(userName,passWord);
+        if(user!=null){
+            resultJson.setStatus(200);
+            resultJson.setMsg("登录成功！");
+            resultJson.setData(user);
+        }else {
+            resultJson.setStatus(500);
+            resultJson.setMsg("用户名或密码错误！");
+        }
+        return resultJson;
+    }
 
 
 
