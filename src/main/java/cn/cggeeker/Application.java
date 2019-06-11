a1f4e28ae920292182ce38554795810c1e7d8064
CGgeeker <2574691263@qq.com>
2019-06-11 11:10:27 +0800

    update  --- something

--- src/main/java/cn/cggeeker/Application.java
+++ src/main/java/cn/cggeeker/Application.java
@@ -1,8 +1,18 @@
 package cn.cggeeker;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import tk.mybatis.spring.annotation.MapperScan;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Auther:CG
@@ -15,5 +25,26 @@ import tk.mybatis.spring.annotation.MapperScan;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
+    }
+
+    /*fastJson调用实现方法*/
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters(){
+        //1. 需要定义一个converter转换消息的对象
+        FastJsonHttpMessageConverter fasHttpMessageConverter = new FastJsonHttpMessageConverter();
+
+        //2. 添加fastjson的配置信息，比如:是否需要格式化返回的json的数据
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+
+        //处理中文乱码问题
+        List<MediaType> fastMediaTypes = new ArrayList<>();
+        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        fasHttpMessageConverter.setSupportedMediaTypes(fastMediaTypes);
+
+        //3. 在converter中添加配置信息
+        fasHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
+        HttpMessageConverter<?> converter = fasHttpMessageConverter;
+        return new HttpMessageConverters(converter);
     }
 }
--- src/main/java/cn/cggeeker/controller/UserContreller.java
+++ src/main/java/cn/cggeeker/controller/UserContreller.java
@@ -5,6 +5,7 @@ import cn.cggeeker.service.UserService;
 import cn.cggeeker.util.ResultJson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class UserContreller {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/userLoginValidate")
+    @GetMapping("/userLoginValidate")
     public ResultJson userLoginValidate(String userName,String passWord){
         ResultJson resultJson = new ResultJson();
         User user = userService.userLoginValidate(userName,passWord);
