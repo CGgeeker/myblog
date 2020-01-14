0530f28cd63dc0c340fbcd334c1bdef9f139bc3b
CGgeeker <2574691263@qq.com>
2020-01-14 10:34:09 +0800

    将springboot项目设置为外部tomcat启动

--- src/main/java/cn/cggeeker/pojo/Article.java
+++ src/main/java/cn/cggeeker/pojo/Article.java
@@ -3,6 +3,7 @@ package cn.cggeeker.pojo;
 import lombok.Data;
 import tk.mybatis.mapper.annotation.KeySql;
 
+import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.sql.Timestamp;
@@ -17,7 +18,7 @@ import java.sql.Timestamp;
 @Table(name = "article_tb") /*将pojo类和数据库对应的表做一个映射*/
 public class Article {
     @Id //指定主键
-    @KeySql(useGeneratedKeys = true)
+    @KeySql(useGeneratedKeys = true)  //开启mysql主键自增策略
     private int articleId;
 
     private String title;
@@ -26,6 +27,7 @@ public class Article {
 
     private String author;
 
+    @Column(name = "readingnum")   //为了避免默认开启的驼峰命名将属性readingNum命名为成字段名为reading_num, 这里就将表字段名readingnum给上
     private int readingNum;
 
     private Timestamp publishTime;
